4cb78d9dae1a20ed336b6b0bf581dd7595d124f0
imenzgolli <imen.zgolli@esprit.tn>
2023-01-07 15:41:25 +0100

    Equipe service

--- src/main/java/tn/spring/springboot/services/EquipeServiceImpl.java
+++ src/main/java/tn/spring/springboot/services/EquipeServiceImpl.java
@@ -1,4 +1,36 @@
 package tn.spring.springboot.services;
+import tn.spring.springboot.entity.Equipe;
+import tn.spring.springboot.repositories.EquipeRepository;
 
-public class EquipeServiceImpl {
+import java.util.List;
+
+public class EquipeServiceImpl  implements IEquipeService{
+
+EquipeRepository equipeRepository;
+    @Override
+    public List<Equipe> retrieveAllEquipes() {
+       return (List<Equipe>) equipeRepository.findAll();
+    }
+
+    @Override
+    public Equipe addEquipe(Equipe equipe) {
+         equipeRepository.save(equipe);
+        return equipe;
+    }
+
+    @Override
+    public Equipe updateEquipe(Equipe equipe) {
+        equipeRepository.save(equipe);
+        return equipe;
+    }
+
+    @Override
+    public Equipe retrieveEquipe(Integer idEquipe) {
+        return equipeRepository.findById(idEquipe).get();
+    }
+
+    @Override
+    public void removeEquipe(Integer idEquipe) {
+        equipeRepository.deleteById(idEquipe);
+    }
 }
--- src/main/java/tn/spring/springboot/services/IEquipeService.java
+++ src/main/java/tn/spring/springboot/services/IEquipeService.java
@@ -1,4 +1,18 @@
 package tn.spring.springboot.services;
 
+import tn.spring.springboot.entity.Equipe;
+
+import java.util.List;
+
 public interface IEquipeService {
+
+    List <Equipe> retrieveAllEquipes();
+    Equipe addEquipe(Equipe equipe);
+    Equipe updateEquipe (Equipe equipe);
+
+    Equipe retrieveEquipe (Integer idEquipe);
+
+    void removeEquipe (Integer idEquipe);
+
+
 }
